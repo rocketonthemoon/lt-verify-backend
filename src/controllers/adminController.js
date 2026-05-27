a7f4bec41ab81d477c51b78b0ecbb6a9ddc16daa
Vartotojas <alonaammu123@gmail.com>
2026-05-27 08:42:15 +0300

    Allow cross-origin cookies with SameSite=None in production

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -7,7 +7,7 @@ const VerificationRequest = require("../models/VerificationRequest");
 
 const COOKIE_OPTS = {
   httpOnly: true,
-  sameSite: "strict",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
   secure: process.env.NODE_ENV === "production",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
